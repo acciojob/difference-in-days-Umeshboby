dd2bfb9e3cdda27cd8365ea5c3efed2c9e8f96f9
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-11-27 12:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const utcDate1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+  const utcDate2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+
+	const timeDiffrenece = utcDate2 - utcDate1;
+	const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  return daysDifference;
 };
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
+
